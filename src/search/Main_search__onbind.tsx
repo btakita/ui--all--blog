f0a172df0e031b93f75c594195f738787778b420
Brian Takita <brian.takita@gmail.com>
2023-10-17 00:31:40 -0400

    Main_search__onbind: fix: type error

--- src/search/Main_search__onbind.tsx
+++ src/search/Main_search__onbind.tsx
@@ -7,7 +7,7 @@ import { UI_search } from './UI_search'
 export const Main_search__onbind = id__dom__handler_(
 	'Main_search__onbind',
 	(Main_search:HTMLElement, ctx:Ctx)=>{
-		const search_item_a = JSON.parse(Main_search.dataset.search_item_a) as SearchItem[]
+		const search_item_a = JSON.parse(Main_search.dataset.search_item_a!) as SearchItem[]
 		render(()=>
 			<UI_search
 				ctx={ctx}
