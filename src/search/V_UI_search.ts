c84f0f789195b43426c17134fd91aa55c4c54f7e
Brian Takita <brian.takita@gmail.com>
2023-11-06 23:51:27 -0500

    V_UI_search:
    	V_results_found: fix: reactivity
    	V_results_children: fix: reactivity
    	input__onkeydown: fix: up/down direction

--- src/search/V_UI_search.ts
+++ src/search/V_UI_search.ts
@@ -1,7 +1,7 @@
 /// <reference lib="dom" />
-import { H_, post__path__new, S_, type SearchItem, SearchResult, vanx_ } from '@btakita/domain--all--blog'
+import { H_, post__path__new, S_, type SearchItem, SearchResult } from '@btakita/domain--all--blog'
 import { type Ctx } from '@ctx-core/object'
-import { reactive_derive__new, reactive_state__new } from '@ctx-core/vanjs'
+import { reactive_derive__new, reactive_state__new, van_ } from '@ctx-core/vanjs'
 import Fuse from 'fuse.js'
 import type { ChildDom, CoreVan } from 'van-type-delegate'
 import { V_card } from '../card'
@@ -11,7 +11,7 @@ export function V_UI_search<van_T extends CoreVan>(
 ):ChildDom<van_T> {
 	const input$ = reactive_state__new<HTMLElement|undefined>(ctx, undefined)
 	const input__value$ = reactive_state__new(ctx, '')
-	const vanx = vanx_(ctx)!
+	const van = van_(ctx)
 	const search_result_a$ = reactive_derive__new(ctx, ()=>[])
 	const search_result_a__length$ = reactive_state__new(ctx, ()=>
 		search_result_a$.length)
@@ -59,6 +59,11 @@ export function V_UI_search<van_T extends CoreVan>(
 	const S = S_(ctx)
 	return (
 		H.div({ class: 'UI_search' },
+			V_input(),
+			V_results_found(),
+			V_results_children())) as ChildDom<van_T>
+	function V_input() {
+		return (
 			H.label({ class: 'relative block' },
 				H.span({ class: 'absolute inset-y-0 left-0 flex items-center pl-2 opacity-75' },
 					S.svg({
@@ -68,19 +73,9 @@ export function V_UI_search<van_T extends CoreVan>(
 						S.path({
 							d: 'M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z'
 						}))),
-				input_()),
-			V_results_found(),
-			()=>
-				H.ul(...search_result_a$.val.map((search_result:SearchResult, idx:number)=>{
-					const item = search_result.item
-					return (
-						V_card({
-							ctx,
-							href: post__path__new(item),
-							class: idx === highlight__idx$.val ? 'highlight' : '',
-							post: item
-						}))
-				})))) as ChildDom<van_T>
+				input_())
+		)
+	}
 	function V_results_found() {
 		const show$ = reactive_derive__new(ctx, ()=>
 			input__value$.val.length > 1)
@@ -95,6 +90,29 @@ export function V_UI_search<van_T extends CoreVan>(
 					input__value$.val)
 				: undefined
 		)
+	}
+	function V_results_children() {
+		let search_result_a:SearchResult[]
+		return (_ul?:HTMLUListElement)=>{
+			const ul:HTMLUListElement = _ul ?? H.ul()
+			rerender()
+			return ul
+			function rerender() {
+				while (ul.lastElementChild) {
+					ul.removeChild(ul.lastElementChild)
+				}
+				van.add(ul, ...search_result_a$.val.map((search_result:SearchResult, idx:number)=>{
+					const item = search_result.item
+					return (()=>
+						V_card({
+							ctx,
+							href: post__path__new(item),
+							class: idx === highlight__idx$.val ? 'highlight' : '',
+							post: item
+						}))
+				}))
+			}
+		}
 	}
 	function input_() {
 		const input =
@@ -116,11 +134,11 @@ export function V_UI_search<van_T extends CoreVan>(
 	}
 	function input__onkeydown(e:KeyboardEvent&{ currentTarget:HTMLInputElement }) {
 		switch (true) {
-			case e.key === 'ArrowUp':
+			case e.key === 'ArrowDown':
 				e.preventDefault()
 				highlight__idx$.val = (highlight__idx$.val + 1) % search_result_a$.val.length
 				break
-			case e.key === 'ArrowDown':
+			case e.key === 'ArrowUp':
 				e.preventDefault()
 				highlight__idx$.val = (highlight__idx$.val + search_result_a$.val.length - 1) % search_result_a$.val.length
 				break
