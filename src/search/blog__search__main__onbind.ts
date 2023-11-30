96c04b751805b046a9e238487218af8d005afef3
Brian Takita <brian.takita@gmail.com>
2023-11-30 14:20:47 -0500

    blog__search__main__onbind: use data-search_item_a prop instead of template
    relementjs: 0.16.1

--- src/search/blog__search__main__onbind.ts
+++ src/search/blog__search__main__onbind.ts
@@ -3,11 +3,11 @@ import { type SearchItem } from '@btakita/domain--any--blog'
 import { id__dom__handler_ } from '@ctx-core/dom'
 import { type Ctx } from 'ctx-core/object'
 import { attach } from 'relementjs'
-import { blog__search_c_ } from './blog__search_c_.ts'
+import { blog__search_c_ } from './blog__search_c_.js'
 export const blog__search__main__onbind = id__dom__handler_(
 	'blog__search__main__onbind',
 	(Main_search:HTMLElement, ctx:Ctx)=>{
 		let search_item_a = JSON.parse(
-			Main_search.querySelector('#blog__search_item_a_c')!.innerHTML) as SearchItem[]
+			Main_search.dataset.search_item_a!) as SearchItem[]
 		attach(Main_search, blog__search_c_({ ctx, search_item_a }))
 	})
