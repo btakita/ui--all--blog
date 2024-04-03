8b3a77b81307dcc12419d0d95061f8f618ee2a0b
Brian Takita <brian.takita@gmail.com>
2024-04-03 00:07:35 -0400

    ./card: blog_author_date_reading_time__div_:
    	img_: ∋ author_a1[0].image
    	author_a1:
    		+ ∋ tb_a_,a_: ∋ author.url
    		render all authors in author_a1

--- card/blog_author_date_reading_time__div.ts
+++ card/blog_author_date_reading_time__div.ts
@@ -1,6 +1,7 @@
 import { blog_post__estimate_read_minutes__new, type dehydrated_post_meta_T } from '@rappstack/domain--any--blog/post'
+import { tb_a_ } from '@rappstack/ui--any/anchor'
 import { class_ } from 'ctx-core/html'
-import { div_, img_, span_ } from 'relementjs/html'
+import { a_, div_, img_, span_ } from 'relementjs/html'
 import { formatted_date_ } from '../date/index.js'
 type blog_author_date_reading_time__div_props_T = {
 	dehydrated_post_meta:dehydrated_post_meta_T
@@ -10,7 +11,7 @@ type blog_author_date_reading_time__div_props_T = {
 }
 export function blog_author_date_reading_time__div_($p:blog_author_date_reading_time__div_props_T) {
 	let { dehydrated_post_meta, blog_post__text, copy_class } = $p
-	const { author, author_img_url, pub_date } = dehydrated_post_meta
+	const { author_a1, pub_date } = dehydrated_post_meta
 	const blog_post__estimate_read_minutes = blog_post__estimate_read_minutes__new(blog_post__text)
 	return (
 		div_({
@@ -21,9 +22,9 @@ export function blog_author_date_reading_time__div_($p:blog_author_date_reading_
 				'text-base',
 				$p.class),
 		}, [
-			img_({
-				src: author_img_url,
-				alt: author,
+			author_a1[0].image && img_({
+				src: author_a1[0].image,
+				alt: author_a1[0].name,
 				class: class_(
 					'inline-block',
 					'w-12',
@@ -35,14 +36,25 @@ export function blog_author_date_reading_time__div_($p:blog_author_date_reading_
 			}),
 			div_({
 				class: class_(
-					'inline-flex',
+					'inline-block',
+					'grow',
 					'flex-col',
 					'ml-4',
 					'text-skin-base',
 					copy_class)
 			}, [
-				span_({ class: 'sr-only' }, 'Author:'),
-				author,
+				span_({ class: 'sr-only' }, `Author${author_a1.length ? 's' : ''}:`),
+				author_a1.map((author, i)=>[
+					i ? ' | ' : '',
+					author.url
+						? (author.a_target_blank ? tb_a_ : a_)({
+							href: author.url,
+							class: class_(
+								'inline'
+							),
+						}, author.name)
+						: author.name
+				]),
 				div_([
 					span_({ class: 'sr-only' }, 'Posted on:'),
 					formatted_date_({ date: pub_date }),
